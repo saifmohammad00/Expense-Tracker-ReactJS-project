e91458bf35162c5349a6e2d2c85dc6a7cd342df5
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-28 20:15:08 +0530

    Login Screen

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,43 +1,66 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./SignUp.module.css";
 const SignUp = () => {
+    const [isLogged, setIsLogged] = useState(false);
+    const [isAuthenticated,setIsAuthentication]=useState(false);
     const handleEmail = useRef();
     const handlePass = useRef();
     const handleCpass = useRef();
     const handleSubmit = async (event) => {
         event.preventDefault();
-        try{
-            const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-UF4eQdtO41w6LL3EcjEC_VrWlhkLZbI', {
-                method: "POST",
+        let url;
+        if (isLogged) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA-UF4eQdtO41w6LL3EcjEC_VrWlhkLZbI';
+        }
+        else {
+            if (handleCpass.current.value !== handlePass.current.value) {
+                alert("Passwords dont match");
+                return;
+            }
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-UF4eQdtO41w6LL3EcjEC_VrWlhkLZbI';
+        }
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
                 body: JSON.stringify({
                     email: handleEmail.current.value,
                     password: handlePass.current.value,
                     returnSecureToken: true,
                 }),
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 }
             })
-            if (res.ok) {
-                console.log("User has Successfully Signed Up")
+            const data = await response.json();
+
+            if (!response.ok) {
+                let errormessage = data.error?.message || "Authentication Failed!";
+                throw new Error(errormessage);
             }
-            else {
-                const data=await res.json();
-                    let errormessage = data.error?.message || "Authentication Failed!";
-                    alert(errormessage);
+            handleEmail.current.value = '';
+            handlePass.current.value = '';
+            if (!isLogged) {
+                handleCpass.current.value = '';
+                setIsLogged((prev)=>!prev);
+            }else{
+                setIsAuthentication(true);
             }
-        }catch(error){
-            console.log(error,'catch block');
+        } catch (error) {
+            console.log(error.message);
         }
+
     }
-return <>
-    <form className={classes.sign} onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <input type="email" id="email" placeholder="Email" ref={handleEmail} required />
-        <input type="password" id="pass" placeholder="Password" ref={handlePass} required />
-        <input type="password" id="cpass" placeholder="Confirm Password" ref={handleCpass} required />
-        <button>Sign Up</button>
-    </form>
-</>
+    return <>
+        {isAuthenticated ? <div>
+            <h1>Welcome to Expense Tracker</h1>
+        </div> : (<form className={classes.sign} onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+            <input type="email" id="email" placeholder="Email" ref={handleEmail} required />
+            <input type="password" id="pass" placeholder="Password" ref={handlePass} required />
+            {!isLogged && <input type="password" id="cpass" placeholder="Confirm Password" ref={handleCpass} required />}
+            <button>{!isLogged ? "Sign Up" : "Login"}</button>
+            <button type="button" onClick={() => setIsLogged(!isLogged)}>{!isLogged ? "Already Have an Account?Login" : "Back to Create Account"}</button>
+        </form>)}
+    </>
 }
 export default SignUp;
